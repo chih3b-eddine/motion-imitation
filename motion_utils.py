15ffb8cba18e8dd27b16402d0657800c55df163e
unknown <mimen4070@gmail.com>
2020-12-31 09:51:01 +0100

    utils update

--- motion_utils.py
+++ motion_utils.py
@@ -1,178 +1,141 @@
 import numpy as np
-from collections import OrderedDict
+import quaternion
+from math import radians
+from mathutils import Matrix, Vector, Quaternion
 
-
-BULLET_SMPL_MAP = OrderedDict({
-    'root'              : 0, # hips   
-    "b'abdomen"         : 3,
-    "b'right_hip"       : 2, 
-    "b'right_knee'"     : 5,
-    "b'right_ankle"     : 8,
-    "b'left_hip"        : 1,
-    "b'left_knee'"      : 4,
-    "b'left_ankle"      : 7,
-    "b'right_shoulder1" : 14,
-    "b'right_shoulder2" : 17,
-    "b'right_elbow'"    : 19,
-    "b'left_elbow'"     : 18,
-    "b'left_shoulder1"  : 13,
-    "b'left_shoulder2"  : 16
-
-})
-
-JOINT_3D_MAP = {
-    "root"   : 39, # hip
-    "l_hand" : 36, # lwrist  
-    "r_hand" : 31, # rwrit
-    "l_foot" : 30, # lankle
-    "r_foot" : 25  # rankle 
+bone_name_from_index = {
+    0 : 'root',  
+    1 : 'L_Hip',
+    2 : 'R_Hip',
+    3 : 'Spine1',     # abdomen
+    4 : 'L_Knee',
+    5 : 'R_Knee',
+    7 : 'L_Ankle',
+    8 : 'R_Ankle',
+    13: 'L_Collar',   # l_shoulder1 
+    14: 'R_Collar',   # r_shoulder1
+    16: 'L_Shoulder',
+    17: 'R_Shoulder',
+    18: 'L_Elbow',
+    19: 'R_Elbow',
 }
 
+bone_names_ordered = ['root', "b'abdomen", 
+                      "b'right_hip", "b'right_knee'", "b'right_ankle",
+                      "b'left_hip",  "b'left_knee'",  "b'left_ankle",
+                      "b'right_shoulder1'", "b'right_shoulder2'", 
+                      "b'right_elbow'",    "b'left_elbow'",
+                      "b'left_shoulder1'",  "b'left_shoulder2'"]
 
+bone_index_ordered = [0, 3, 
+                      2, 5, 8,
+                      1, 4, 7,
+                      14, 17, 
+                      19, 18, 
+                      13, 16]
+
+
+# switch axis [x, y, z] -> [y, z, x]
+def convert_position(pos):
+    return [pos[1], pos[2], pos[0]]
+
+
+# Computes rotation matrix through Rodrigues formula as in cv2.Rodrigues
+# Source: smpl/plugins/blender/corrective_bpy_sh.py
+def Rodrigues(rotvec):
+    theta = np.linalg.norm(rotvec)
+    r = (rotvec/theta).reshape(3, 1) if theta > 0. else rotvec
+    cost = np.cos(theta)
+    mat = np.asarray([[0, -r[2], r[1]],
+                      [r[2], 0, -r[0]],
+                      [-r[1], r[0], 0]])
+    return(cost*np.eye(3) + (1-cost)*r.dot(r.T) + np.sin(theta)*mat)
+
+  
+# compute the angle of an angle-axis
 def aa_to_angle(aa):
-    """
-        aa = [w.theta] axis-angle -> theta (in radians)
-    """ 
     return np.linalg.norm(aa)
 
 
-def aa_to_quat(aa):
-    """
-        aa = [w.theta] axis-angle -> (s, x, y, z) unit quaternion 
-    """
-    angle = aa_to_angle(aa)
-    w = np.true_divide(aa,(angle + 1e-8))   # avoid 0 angle
-    s = np.cos(angle*0.5)
-    w *= np.sin(angle*0.5)
-    return np.insert(w, 0, s)
+def process_pose(pose):
+    angles = []
+    for index in bone_index_ordered:
+        aa = pose[index,:]
 
-
-def quat_mult(q1, q2):
-    """
-        quaternions q1, q2  -> q1 x q2
-    """
-    s1, x1, y1, z1 = q1[0], q1[1], q1[2], q1[3]
-    s2, x2, y2, z2 = q2[0], q2[1], q2[2], q2[3]
-    
-    x =  x1 * s2 + y1 * z2 - z1 * y2 + s1 * x2;
-    y = -x1 * z2 + y1 * s2 + z1 * x2 + s1 * y2;
-    z =  x1 * y2 - y1 * x2 + z1 * s2 + s1 * z2;
-    s = -x1 * x2 - y1 * y2 - z1 * z2 + s1 * s2;
-    return [s, x, y, z]
-
-
-def quat_conj(q):
-    """
-        quaternion q  = (s, x, y, z)  -> q* = (s, -x, -y, -z)
-    """
-    s, x, y, z = q
-    return [s, -x, -y, -z]
-
-
-def compute_rotations(pose):
-    """
-    """
-    v = []
-    a = []
-    
-    ry = [0.7071, 0, 0.7071, 0]    # Quaternion that represents 90 degrees around Y
-    rz = [0.7071, 0, 0, 0.7071]    # Quaternion that represents 90 degrees around Z
-    
-    rz_ = quat_conj(rz)
-    ry_ = quat_conj(ry)
-    
-    for j, j_key in  BULLET_SMPL_MAP.items():
-        aa = np.asarray(pose[j_key,:])
-        if (j == 'root'):               # the bone towards -y / down
-            q = aa_to_quat(aa)
-            q = quat_mult(ry, q)              
-            q = quat_mult(q, ry_)  
-            q = quat_mult([0, 1, 0, 0], q)
-            root_orientation = q
+        quat_x_90_cw = Quaternion((1.0, 0.0, 0.0), radians(-90))  # -90 degrees rotation around X
+        quat_y_90_cw = Quaternion((0.0, 1.0, 0.0), radians(-90))  # -90 degrees rotation around Y
+        quat_z_90_cw = Quaternion((0.0, 0.0, 1.0), radians(-90))  # -90 degrees rotation around Z
+        quat_yn_90_cw = Quaternion((0.0, 1.0, 0.0), radians(90))  # +90 degrees rotation around Y
         
-        elif (j == "b'abdomen"):        # the bone towards -z 
-            q = aa_to_quat(aa)
-            q = quat_mult(ry, q)
-            s, x, y, z = quat_mult(q, ry_)  
-            a.extend([z, y, x])
-            v.append(s)
-            
-        elif (j == "b'left_hip"):       # the bone towards +x 
-            q = aa_to_quat(aa)
-            q = quat_mult(q, rz)
-            q = quat_mult(ry, q)
-            s, x, y, z  = quat_mult(q, ry_)
-            a.extend([x, z, y])
-            v.append(s)
-            
-        elif (j == "b'right_hip"):      # the bone towards -x 
-            q = aa_to_quat(aa)
-            q = quat_mult(q, rz_)
-            q = quat_mult(ry, q)
-            s, x, y, z = quat_mult(q, ry_)
-            a.extend([x, z, y])
-            v.append(s)
-
-        elif (j == "b'left_shoulder1"): # the bone towards +x 
-            q = aa_to_quat(aa)
-            q = quat_mult(q, rz)
-            q = quat_mult(ry, q)
-            s, x, y, z = quat_mult(q, ry_)
-            a.extend([x])
-            v.append(s)
-            
-        elif (j == "b'left_shoulder2"): # the bone towards +x 
-            q = aa_to_quat(aa)
-            q = quat_mult(q, rz)
-            q = quat_mult(ry, q)
-            s, x, y, z = quat_mult(q, ry_)
-            a.extend([x])
-            v.append(s)
-            
-        elif (j == "b'right_shoulder1"): # the bone towards -x 
-            q = aa_to_quat(aa)
-            q = quat_mult(q, rz_)
-            q = quat_mult(ry, q)
-            s, x, y, z = quat_mult(q, ry_)
-            a.extend([x])
-            v.append(s)
-            
-        elif (j == "b'right_shoulder2"): # the bone towards -x 
-            q = aa_to_quat(aa)
-            q = quat_mult(q, rz_)
-            q = quat_mult(ry, q)
-            s, x, y, z = quat_mult(q, ry_)
-            a.extend([x])
-            v.append(s)
-
-        elif (j == "b'left_ankle"):      # the bone towards +x  (error)
-            q = aa_to_quat(aa)
-            q = quat_mult(q, rz)
-            q = quat_mult(ry, q)
-            s, x, y, z = quat_mult(q, ry)
-            a.extend([y, x])
-            v.append(s)
-            #print(x, y, z)
-            
-        elif (j == "b'right_ankle"):       # the bone towards -x (error) 
-            q = aa_to_quat(aa)
-            q = quat_mult(q, rz_)
-            q = quat_mult(ry, q)
-            s, x, y, z = quat_mult(q, ry_)
-            a.extend([y, x])
-            v.append(s)
-       
-        elif (j in ["b'right_elbow'", "b'left_elbow'"]):
-            s = aa_to_angle(aa)
-            a.extend([s])
-            v.append(s)
-            
-        elif (j in ["b'right_knee'", "b'left_knee'"]):
-            s = aa_to_angle(aa)
-            a.extend([-s])
-            v.append(-s)
+        if index == 0 :  
+            # ---------- root : ZXY to XYZ --------------------#
+            # -----------------------------------------
+            # SMPL Root: (x, y, z) = (right, up, out)   
+            # BULLET Root: (x, y, z) = (out, right, up)  
+            # -----------------------------------------
+            bone_rotation = Matrix(Rodrigues(aa)).to_quaternion()
+            q = (quat_z_90_cw @ quat_x_90_cw) @ bone_rotation
+            root_orientation = [q.w, q.x, q.y, q.z]
         
-    return root_orientation, a, v
-              
+            #---------------- TOP PARTS ------------------------#
+        elif index == 3:
+            # ---------- abdomen : rotation vector +Y ----------#
+            bone_rotation = Matrix(Rodrigues(aa)).to_quaternion()
+            q = (quat_yn_90_cw) @ bone_rotation             
+            angles.append([q.z])
+            angles.append([q.y])
+            angles.append([q.x])
 
-              
+        elif index == 1:
+            # ---------- l-shoulder : rotation vector -Y -------#
+            bone_rotation = Matrix(Rodrigues(aa)).to_quaternion()
+            q = quat_y_90_cw @ bone_rotation
+            q = (quat_z_90_cw @ quat_x_90_cw) @ q
+            angles.append([q.x])
+            angles.append([q.z])
+            angles.append([q.y])
+
+        elif index == 2:
+            # ---------- r-shoulder : rotation vector +Y------#
+            bone_rotation = Matrix(Rodrigues(aa)).to_quaternion()
+            q = quat_yn_90_cw @ bone_rotation
+            q = (quat_z_90_cw @ quat_x_90_cw) @ q
+            angles.append([q.x])
+            angles.append([q.z])
+            angles.append([q.y])
+            
+            #------------------- DOWN PARTS ------------------#
+        elif index == 1:
+            # ---------- l-hip : rotation vector -Y ----------#
+            bone_rotation = Matrix(Rodrigues(aa)).to_quaternion()
+            q = quat_y_90_cw @ bone_rotation
+            q = (quat_z_90_cw @ quat_x_90_cw) @ q
+            angles.append([q.x])
+            angles.append([q.z])
+            angles.append([q.y])
+
+        elif index == 2:
+            # ---------- r-hip : rotation vector +Y ----------#
+            bone_rotation = Matrix(Rodrigues(aa)).to_quaternion()
+            q = quat_yn_90_cw @ bone_rotation
+            q = (quat_z_90_cw @ quat_x_90_cw) @ q
+            angles.append([q.x])
+            angles.append([q.z])
+            angles.append([q.y])
+            
+        elif index in [4, 5]:
+            # ---------- knees : rotation -Y -----------------#
+            q = aa_to_angle(aa)
+            angles.append([- q]) 
+        
+        elif index in [7, 8]:
+            # ---------- ankles  ----------#
+            q = Matrix(Rodrigues(aa)).to_quaternion()
+            q = (quat_z_90_cw @ quat_x_90_cw) @ q
+            angles.append([q.y])
+            angles.append([q.x])
+        else:
+            q = aa_to_angle(aa)
+            angles.append([q])                  
+        
+    return root_orientation, angles
