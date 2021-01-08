a766b65dfe05fe38324026a5de7df7bb21a872bb
unknown <mimen4070@gmail.com>
2021-01-08 21:38:20 +0100

    rotate 3d positions automatically

--- motion_utils.py
+++ motion_utils.py
@@ -1,7 +1,6 @@
 import numpy as np
 from math import radians
 from mathutils import Matrix, Vector, Quaternion
-import mathutils as mt
 
 
 # To compute Orientations from "VIBE-SMPL Poses"
@@ -24,7 +23,6 @@ positions_ids_ordered = [40,  # thorax
                          31,  # rwrist
                          21,  # OP LHeel    30, # lankle
                          24]  # OP RHeel    25  # rankle
-
 
 
 # Computes rotation matrix through Rodrigues formula as in cv2.Rodrigues, Source: smpl/plugins/blender/corrective_bpy_sh.py
@@ -51,13 +49,7 @@ def quat_invert(q):
     return Quaternion((q.w, -q.x, -q.y, -q.z))
 
 
-# switch axis (x, y, z) -> (y, z, x)
-def convert_position(pos):
-    x, y, z = pos
-    return np.asarray([y, z, x])
-
-
-def compute_positions(joints3d, root0, chest0):
+def compute_positions(joints3d, rotation_matrix,  root0, chest0):
     """
         joints3d: (n_frames, 49, 3) SMPL 3D joints position in real world    
     returns:
@@ -65,18 +57,17 @@ def compute_positions(joints3d, root0, chest0):
             - mapping the SMPL-root to BULLET-chest (the chest is the origin of BULLET space) at the first timestep
             - computing the position of each joint shifted by the translation of the mapped SMPL-root 
     """
-    positions = []
+    shift = np.asarray([0, 0, 1.3])
     
-    shift = np.asarray([0, 0, 0])
-    root0 = convert_position(root0)
-    chest0 = convert_position(chest0)
-    root_position = convert_position(joints3d[0])
-   
+    root0 = rotation_matrix @ root0
+    chest0 = rotation_matrix @ chest0
+    root_position = rotation_matrix @ joints3d[0]
+    
+    positions = []
     for index in positions_ids_ordered:                     
-        joint_position = convert_position(joints3d[index]) 
+        joint_position = rotation_matrix @ joints3d[index]
         positions.append(list(joint_position + root_position - root0 - chest0 + shift))   
     return positions
-
 
 
 def compute_orientations(pose):
@@ -87,48 +78,47 @@ def compute_orientations(pose):
     returns:
         mapping of VIBE-SMPL body pose to Bullet orientations:
             - change the origin of space from VIBE-root to BULLET-chest
-            - change the global orientation from VIBE coordinate system (YZX) to Bullet coordinate system (XYZ), both expressed as
-              (out, right, up) in right hand system
-            - the joints orientation is already expressed relatively to their parents, so no need to transform them        
+            - change the global orientation from VIBE coordinate system to Bullet coordinate system 
+            - change joints orientation to new local parents        
     """
     pose = pose.reshape(-1,3)
     
     rotation_x_ = Quaternion((1.0, 0.0, 0.0), radians(-90))
     rotation_y_ = Quaternion((0.0, 1.0, 0.0), radians(-90))
-    rotation_z_ = Quaternion((0.0, 0.0, 1.0), radians(-90))
-
-    
-    G_root = Matrix(Rodrigues( pose[0,:])).to_quaternion()      # root global orientation
-    q_spine = Matrix(Rodrigues( pose[3,:])).to_quaternion()
-    q_spine_1 = Matrix(Rodrigues( pose[6,:])).to_quaternion()
-    q_spine_2 = Matrix(Rodrigues( pose[9,:])).to_quaternion()
-    G_chest = G_root @ (q_spine @ (q_spine_1 @ q_spine_2))      # chest global orientation
-    
-    G_chest_inv = quat_invert(G_chest)
-    
-    global_rotation = ( quat_invert(rotation_z_) @ rotation_z_)
+    global_rotation = rotation_y_ @   rotation_x_
     
     angles = []
     for index in bone_index_ordered:
-        aa = pose[index,:]           # joint pose in axis-angles representation
+        aa = pose[index,:]                                   # joint pose in axis-angles representation
         q = Matrix(Rodrigues(aa)).to_quaternion()
         
         if index == 9 :              # chest 
+            
+            G_root = Matrix(Rodrigues( pose[0,:])).to_quaternion()      # original root global orientation
+            q_spine = Matrix(Rodrigues( pose[3,:])).to_quaternion()
+            q_spine_1 = Matrix(Rodrigues( pose[6,:])).to_quaternion()
+            q_spine_2 = Matrix(Rodrigues( pose[9,:])).to_quaternion()
+            G_chest = G_root @ (q_spine @ (q_spine_1 @ q_spine_2))      # original chest global orientation
+            
             q = global_rotation @ G_chest                    # mathutils quaternion order = [w, x, y, z]
             root_orientation = [q.x, q.y, q.z, q.w]          # bullet quaternion order = [x, y, z, w]
             
+            G_chest_inv = quat_invert(q) 
+            global_rotation_matrix = np.array(G_chest.to_matrix())
+            
         elif index == 3:             # abdomen /// bullet (z, y, x) 
-            q = G_chest_inv @ (G_root @ q)
-            q = global_rotation @ q
+            q = global_rotation @ (G_root @ q)
+            q = G_chest_inv @ q
             aa = quat_to_aa(q)
+            G_abdomen_inv = quat_invert(q)
             
             angles.append([aa[2]])
             angles.append([aa[1]])
             angles.append([aa[0]])
 
         elif index == 2:             # r-hip /// bullet (x, z, y)
-            q = G_chest_inv @ (G_root @ q)
-            q = global_rotation @ q
+            q = global_rotation @ (G_root @ q)
+            q = G_abdomen_inv @ q
             aa = quat_to_aa(q)
             
             angles.append([aa[0]])
@@ -136,8 +126,8 @@ def compute_orientations(pose):
             angles.append([aa[1]])
             
         elif index == 1:             # l-hip /// bullet (x, z, y) 
-            q = G_chest_inv @ (G_root @ q)
-            q = global_rotation @ q
+            q = global_rotation @ (G_root @ q)
+            q = G_abdomen_inv @ q
             aa = quat_to_aa(q)
             
             angles.append([aa[0]])   
@@ -146,21 +136,21 @@ def compute_orientations(pose):
             
         elif index == 17:            # r-shoulder /// bullet (x, y) 
             q_parent = Matrix(Rodrigues( pose[14,:])).to_quaternion()
-            q = G_chest_inv @ (q_spine_2 @ (q_parent @ q))
-            q = global_rotation @ q
+            q = global_rotation @ (q_spine_2 @ (q_parent @ q))
+            q = G_chest_inv @ q
             aa = quat_to_aa(q)
             
-            angles.append([aa[0]])
-            angles.append([aa[1]])    
+            angles.append([aa[0]+0.75])
+            angles.append([aa[1]-0.4])    
             
         elif index == 16:            # l-shoulder /// bullet (x, y) 
             q_parent = Matrix(Rodrigues( pose[13,:])).to_quaternion()
-            q = G_chest_inv @ (q_spine_2 @ (q_parent @ q))
-            q = global_rotation @ q
+            q = global_rotation @ (q_spine_2 @ (q_parent @ q))
+            q = G_chest_inv @ q
             aa = quat_to_aa(q)
             
-            angles.append([aa[0]])
-            angles.append([aa[1]])
+            angles.append([aa[0]-0.75])
+            angles.append([aa[1]+0.4])
             
         elif index in [8, 7]:        # ankles   /// bullet (y, x) 
             q = global_rotation @ q
@@ -174,6 +164,6 @@ def compute_orientations(pose):
             
         elif index in [19, 18]:       # elbows
             theta = aa_to_angle(aa)
-            angles.append([theta])              
-        
-    return root_orientation, angles
+            angles.append([theta-1.7])              
+    
+    return root_orientation, angles, global_rotation_matrix
