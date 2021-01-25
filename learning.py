22a0b1905f21f77a953f8bd3d5581b1a026e1f71
Chiheb <chihebnajjar22@gmail.com>
2021-01-25 02:06:24 +0100

    split samples into trajectories to compute better the GAE

--- learning.py
+++ learning.py
@@ -6,12 +6,63 @@ import json
 import random
 import numpy as np
 
+class Buffer:
+    def __init__(self):
+        self.data = [[]]
+
+    def __len__(self):
+        l = 0
+        for trajectory in self.data:
+            l += len(trajectory)
+        return l
+
+    def append(self, d, new_trajectory=False):
+        if new_trajectory:
+            self.data.append([d])
+        else:
+            self.data[-1].append(d)
+
+    def get_data(self):
+        # flatten the data
+        return sum(self.data, [])
+
+
+class RLBuffer:
+    def __init__(self):
+        self.states = Buffer()
+        self.actions = Buffer()
+        self.rewards = Buffer()
+        self.log_probs = Buffer()
+        self.values = Buffer()
+        self.advantages = Buffer()
+    
+    def __len__(self):
+        return len(self.values)
+
+    def append(self, state, action, reward, log_prob, value, new_trajectory=False):
+        self.states.append(state, new_trajectory)
+        self.actions.append(action, new_trajectory)
+        self.rewards.append(reward, new_trajectory)
+        self.log_probs.append(log_prob, new_trajectory)
+        self.values.append(value, new_trajectory)
+
+    def apply_GAE(self, Gamma=0.95, Lambda=0.95):
+        self.advantages.data = []
+        for i in range(len(self.values.data)):
+            advantages = GAE(self.values.data[i], self.rewards.data[i], Gamma=Gamma, Lambda=Lambda)
+            self.advantages.data.append(advantages)
+
+    def get_data(self):
+        return (self.states.get_data(), self.actions.get_data(), self.rewards.get_data(), 
+            self.log_probs.get_data(), self.values.get_data(), self.advantages.get_data())
+
 
 def append_to_file(data, path): 
     with open(path, 'a') as outfile:
         outfile.write(json.dumps(data))
         outfile.write(",")
         outfile.close() 
+
 
 def GAE(values, rewards, Gamma=0.95, Lambda=0.95):
     T = len(rewards)
@@ -59,20 +110,15 @@ def state_to_tensor(state):
     return T
 
 
-
-
 def train(reference_motion, Gamma=0.95, Lambda=0.95, n_episodes=1000, n_steps=500, minibatch_size=256, update_every=4096, n_updates=20, epsilon=0.2, test_every=5, test_episodes=10):
     frames = reference_motion["frames"]
     n_frames = len(frames)
     best_reward = 10.0
 
     for episode in range(n_episodes):
-        log_probs = []
-        values = []
-        states = []
-        actions = []
-        rewards = []
-        while len(values) < update_every:
+        buffer = RLBuffer()
+        new_trajectory = False
+        while len(buffer) < update_every:
             t0 = random.choice(range(n_frames//2))
             state = env.reset(frames[t0])
             for t in range(t0, min(t0 + n_steps, n_frames)):
@@ -84,21 +130,20 @@ def train(reference_motion, Gamma=0.95, Lambda=0.95, n_episodes=1000, n_steps=50
                 action = policy.sample()
                 next_state, reward = env.step(action.cpu().numpy()[0,:], frames[t])
                 log_prob = policy.log_prob(action)
-                
-                log_probs.append(log_prob)
-                values.append(value)
-                states.append(state)
-                actions.append(action)
-                rewards.append(torch.FloatTensor([reward]).to(device))
+
+                buffer.append(state, action, torch.FloatTensor([reward]).to(device), log_prob, value, new_trajectory=new_trajectory)
                 append_to_file(reward, path="data/allrewards.txt")
         
                 if next_state["isTerminal"]:
+                    new_trajectory = True
                     break
                 else:
                     state = next_state
+                    new_trajectory = False
+
+        buffer.apply_GAE()
+        states, actions, rewards, log_probs, values, advantages_GAE = buffer.get_data()
                 
-        advantages_GAE = GAE(values, rewards, Gamma=Gamma, Lambda=Lambda)
-        
         advantages_GAE = torch.cat(advantages_GAE).detach()
         log_probs = torch.cat(log_probs).detach()
         values = torch.cat(values).detach()
@@ -120,8 +165,7 @@ def train(reference_motion, Gamma=0.95, Lambda=0.95, n_episodes=1000, n_steps=50
                 best_reward = avg_test_reward
 
 
-
-def PPO(advantages_GAE, log_probs, values_TD, states, actions, minibatch_size=256, n_updates=20, epsilon=0.2):
+def PPO(advantages_GAE, log_probs, values_TD, states, actions, minibatch_size=256, n_updates=20, epsilon=0.2, pg=True):
     for _ in range(n_updates):
         rand_ids = np.random.randint(0, len(states), minibatch_size)
         A_GAE = advantages_GAE[rand_ids,:]
@@ -136,11 +180,15 @@ def PPO(advantages_GAE, log_probs, values_TD, states, actions, minibatch_size=25
         new_lp = new_policies.log_prob(a)
         
         Poptimizer.zero_grad()
-        #W = (new_lp - lp).exp()
-        #term1 = W * A_GAE
-        #term2 = torch.clamp(W, 1.0 - epsilon, 1.0 + epsilon) * A_GAE
-        #policy_loss  = - torch.min(term1, term2).mean()
-        policy_loss  = - (new_lp * A_GAE).mean()
+
+        if pg:
+            policy_loss  = - (new_lp * A_GAE).mean()
+        else:
+            W = (new_lp - lp).exp()
+            term1 = W * A_GAE
+            term2 = torch.clamp(W, 1.0 - epsilon, 1.0 + epsilon) * A_GAE
+            policy_loss  = - torch.min(term1, term2).mean()
+        
         policy_loss.backward()
         Poptimizer.step()
         
